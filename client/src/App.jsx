db26a153c63923fbd0466ad14117f1c53c35172d
sohanmali454 <malisohan454@gmail.com>
2024-12-11 01:44:27 +0530

    create pages & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminSignIn from "./pages/AdminSignIn";
+import UserRegistration from "./pages/UserRegistration";
+import AdminDashboard from "./pages/AdminDashboard";
+import PaymentGetway from "./pages/PaymentGetway";
+import RegistrationSuccessful from "./pages/RegistrationSuccessful";
 export default function App() {
   return (
-    <div>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin-sign-in" element={<AdminSignIn />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/user-registration" element={<UserRegistration />} />
+        <Route path="/payment-getway" element={<PaymentGetway />} />
+        <Route
+          path="/registration-successful"
+          element={<RegistrationSuccessful />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/PaymentGetway.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function PaymentGetway() {
+  return <div>PaymentGetway</div>;
+}
